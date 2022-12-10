b5ebc80b0317b4e773cb520beed9f317aedd3284
KIlnur <kilnur1988@rambler.ru>
2022-12-10 12:59:17 +0500

    Поправил авторизацию на сайте и редирект на главную страницу

--- project/src/pages/login-screen/login-screen.tsx
+++ project/src/pages/login-screen/login-screen.tsx
@@ -1,16 +1,17 @@
 import Logo from '../../components/logo/logo';
 import { loginAction } from '../../store/api-actions';
-import { useAppDispatch } from '../../hooks/index';
-import { FormEvent, useRef } from 'react';
+import { useAppDispatch, useAppSelector } from '../../hooks/index';
+import { FormEvent, useRef, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { AuthData } from '../../types/auth-data';
-import { AppRoute } from '../../const';
+import { AppRoute, AuthorizationStatus } from '../../const';
 
 function LoginScreen(): JSX.Element {
   const emailRef = useRef<HTMLInputElement | null>(null);
   const passwordRef = useRef<HTMLInputElement | null>(null);
   const dispatch = useAppDispatch();
   const navigate = useNavigate();
+  const authStatus = useAppSelector((state) => state.authorizationStatus);
 
   const onSubmit = (authData: AuthData) => {
     dispatch(loginAction(authData));
@@ -20,14 +21,18 @@ function LoginScreen(): JSX.Element {
     evt.preventDefault();
 
     if (emailRef !== null && passwordRef !== null) {
-      onSubmit ({
+      onSubmit({
         email: emailRef.current?.value,
         password: passwordRef.current?.value,
       });
     }
   };
 
-  const handleSignOutClick = () => navigate(AppRoute.Root);
+  useEffect(() => {
+    if (authStatus === AuthorizationStatus.Auth) {
+      navigate(AppRoute.Root);
+    }
+  }, [authStatus, navigate]);
 
   return (
     <div className="user-page">
@@ -43,16 +48,16 @@ function LoginScreen(): JSX.Element {
         <form action="" className="sign-in__form" onSubmit={handleSubmit}>
           <div className="sign-in__fields">
             <div className="sign-in__field">
-              <input className="sign-in__input" type="email" placeholder="Email address" name="user-email" id="user-email" ref={emailRef}/>
+              <input className="sign-in__input" type="email" placeholder="Email address" name="user-email" id="user-email" ref={emailRef} />
               <label className="sign-in__label visually-hidden" htmlFor="user-email">Email address</label>
             </div>
             <div className="sign-in__field">
-              <input className="sign-in__input" type="password" placeholder="Password" name="user-password" id="user-password" ref={passwordRef}/>
+              <input className="sign-in__input" type="password" placeholder="Password" name="user-password" id="user-password" ref={passwordRef} />
               <label className="sign-in__label visually-hidden" htmlFor="user-password">Password</label>
             </div>
           </div>
           <div className="sign-in__submit">
-            <button className="sign-in__btn" type="submit" onClick={handleSignOutClick}>Sign in</button>
+            <button className="sign-in__btn" type="submit">Sign in</button>
           </div>
         </form>
       </div>
